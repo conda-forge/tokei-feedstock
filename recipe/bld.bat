e9d93e24d9248f4b6eb99f0468c54cf80b1a1c32
Daniel Bast <2790401+dbast@users.noreply.github.com>
2018-09-22 22:28:59 +0200

    Strip debug symbols
    Reduces binary size by ca. 16%.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,6 +2,8 @@
 cargo build   --features all --release         || goto :error
 cargo install --features all --root "%PREFIX%" || goto :error
 
+:: strip debug symbols
+strip "%PREFIX%\bin\tokei.exe" || goto :error
 goto :EOF
 
 :error
