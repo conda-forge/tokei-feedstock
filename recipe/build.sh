97f0dc44c9869913891ca2dc196f0f2eed3c61ed
Daniel Bast <2790401+dbast@users.noreply.github.com>
2019-01-14 07:48:42 +0100

    Update install arguments

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,7 +4,7 @@ set -o xtrace -o nounset -o pipefail -o errexit
 
 # build statically linked binary with Rust
 cargo build --features all --release
-cargo install --features all --root "$PREFIX"
+cargo install --features all --root "$PREFIX" --path .
 
 # strip debug symbols
 strip "$PREFIX/bin/tokei"
