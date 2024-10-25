ff4dc1897c0ee2caf582504ec40f95d07847c61e
Daniel Bast <2790401+dbast@users.noreply.github.com>
2024-10-25 17:12:47 +0200

    Update to 12.1.2 + modernize recipe

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,11 +2,12 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
+export CARGO_PROFILE_RELEASE_STRIP=symbols
+export CARGO_PROFILE_RELEASE_LTO=fat
+
+cargo-bundle-licenses \
+    --format yaml \
+    --output THIRDPARTY.yml
+
 # build statically linked binary with Rust
-cargo install --locked --features all --root "$PREFIX" --path .
-
-# strip debug symbols
-"$STRIP" "$PREFIX/bin/tokei"
-
-# remove extra build file
-rm -f "${PREFIX}/.crates.toml"
+cargo install --no-track --locked --features all --root "$PREFIX" --path .
